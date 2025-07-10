20869c100af2cc64159855c6e88b60d0e90ba773
Paras Jadav <99166408@brighthorizons.com>
2025-07-10 16:41:50 +0530

    Added try catch and exception handling code

--- TariffComparison/Services/TariffService.cs
+++ TariffComparison/Services/TariffService.cs
@@ -14,63 +14,70 @@ public class TariffService
     {
         var filteredResults = new List<TariffModel>();
 
-        if (tarifftype.Equals(Constants.TariffTypeAll, StringComparison.OrdinalIgnoreCase))
+        try
         {
-            filteredResults = cachedRows.Select(row => new TariffModel
+            if (tarifftype.Equals(Constants.TariffTypeAll, StringComparison.OrdinalIgnoreCase))
             {
-                TariffName = row.TryGetValue(Constants.ProductName, out var nameValue) && nameValue != null
-                    ? nameValue.ToString()
-                    : string.Empty,
-                TariffType = row.TryGetValue(Constants.TariffType, out var typeValue) && typeValue != null
-                    ? (typeValue.ToString() == Constants.TariffTypeBasic ? Constants.TariffTypeBasicName : Constants.TariffTypePackagedName)
-                    : string.Empty,
-                AnnualCost = CalculateAnnualCost(row, tarifftype, consumption)
-            }).ToList();
-        }
-        else if (!string.IsNullOrEmpty(tarifftype))
-        {
-            filteredResults = cachedRows.Where(row => row[Constants.TariffType]?.ToString() == tarifftype).Select(row => new TariffModel
+                filteredResults = cachedRows.Select(row => new TariffModel
+                {
+                    TariffName = row.TryGetValue(Constants.ProductName, out var nameValue) && nameValue != null
+                        ? nameValue.ToString()
+                        : string.Empty,
+                    TariffType = row.TryGetValue(Constants.TariffType, out var typeValue) && typeValue != null
+                        ? (typeValue.ToString() == Constants.TariffTypeBasic ? Constants.TariffTypeBasicName : Constants.TariffTypePackagedName)
+                        : string.Empty,
+                    AnnualCost = CalculateAnnualCost(row, tarifftype, consumption)
+                }).ToList();
+            }
+            else if (!string.IsNullOrEmpty(tarifftype))
             {
-                TariffName = row.TryGetValue(Constants.ProductName, out var nameValue) && nameValue != null
-                    ? nameValue.ToString()
-                    : string.Empty,
-                TariffType = row.TryGetValue(Constants.TariffType, out var typeValue) && typeValue != null
-                    ? (typeValue.ToString() == Constants.TariffTypeBasic ? Constants.TariffTypeBasicName : Constants.TariffTypePackagedName)
-                    : string.Empty,
-                AnnualCost = CalculateAnnualCost(row, tarifftype, consumption)
-            }).ToList();
-        }
+                filteredResults = cachedRows.Where(row => row[Constants.TariffType]?.ToString() == tarifftype).Select(row => new TariffModel
+                {
+                    TariffName = row.TryGetValue(Constants.ProductName, out var nameValue) && nameValue != null
+                        ? nameValue.ToString()
+                        : string.Empty,
+                    TariffType = row.TryGetValue(Constants.TariffType, out var typeValue) && typeValue != null
+                        ? (typeValue.ToString() == Constants.TariffTypeBasic ? Constants.TariffTypeBasicName : Constants.TariffTypePackagedName)
+                        : string.Empty,
+                    AnnualCost = CalculateAnnualCost(row, tarifftype, consumption)
+                }).ToList();
+            }
 
-        if (requestData.TryGetValue(Constants.RequestSortBy, out var sortBy))
-        {
-            var sortByValue = sortBy?.ToString();
-            switch (sortByValue?.ToLower())
+            if (requestData.TryGetValue(Constants.RequestSortBy, out var sortBy))
             {
-                case "priceasc":
-                    filteredResults = filteredResults.OrderBy(t => t.AnnualCost).ToList();
-                    break;
-                case "pricedesc":
-                    filteredResults = filteredResults.OrderByDescending(t => t.AnnualCost).ToList();
-                    break;
-                case "nameasc":
-                    filteredResults = filteredResults.OrderBy(t => t.TariffName).ToList();
-                    break;
-                case "namedesc":
-                    filteredResults = filteredResults.OrderByDescending(t => t.TariffName).ToList();
-                    break;
-                case "typeasc":
-                    filteredResults = filteredResults.OrderBy(t => t.TariffType).ToList();
-                    break;
-                case "typedesc":
-                    filteredResults = filteredResults.OrderByDescending(t => t.TariffType).ToList();
-                    break;
-                default:
-                    logger.LogWarning("Invalid sortBy value provided. No sorting applied.");
-                    break;
+                var sortByValue = sortBy?.ToString();
+                switch (sortByValue?.ToLower())
+                {
+                    case "priceasc":
+                        filteredResults = filteredResults.OrderBy(t => t.AnnualCost).ToList();
+                        break;
+                    case "pricedesc":
+                        filteredResults = filteredResults.OrderByDescending(t => t.AnnualCost).ToList();
+                        break;
+                    case "nameasc":
+                        filteredResults = filteredResults.OrderBy(t => t.TariffName).ToList();
+                        break;
+                    case "namedesc":
+                        filteredResults = filteredResults.OrderByDescending(t => t.TariffName).ToList();
+                        break;
+                    case "typeasc":
+                        filteredResults = filteredResults.OrderBy(t => t.TariffType).ToList();
+                        break;
+                    case "typedesc":
+                        filteredResults = filteredResults.OrderByDescending(t => t.TariffType).ToList();
+                        break;
+                    default:
+                        logger.LogWarning("Invalid sortBy value provided. No sorting applied.");
+                        break;
+                }
             }
         }
-
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error occurred while filtering tariffs.");
+        }
         return filteredResults;
+
     }
 
     public double CalculateAnnualCost(Dictionary<string, object> row, string tarifftype, string consumption)
@@ -78,93 +85,100 @@ public class TariffService
         double basicCost = 0.00;
         double consumptionCost = 0.00;
 
-        if (!string.IsNullOrEmpty(consumption))
+        try
         {
-            if (tarifftype.Equals(Constants.TariffTypeAll))
+            if (!string.IsNullOrEmpty(consumption))
             {
-                if (row.TryGetValue(Constants.TariffType, out var type))
+                if (tarifftype.Equals(Constants.TariffTypeAll))
                 {
-                    if (!string.IsNullOrEmpty(type.ToString()))
+                    if (row.TryGetValue(Constants.TariffType, out var type))
                     {
-                        if (type.ToString().Equals(Constants.TariffTypeBasic, StringComparison.OrdinalIgnoreCase))
+                        if (!string.IsNullOrEmpty(type.ToString()))
+                        {
+                            if (type.ToString().Equals(Constants.TariffTypeBasic, StringComparison.OrdinalIgnoreCase))
+                            {
+                                if (row.TryGetValue(Constants.BaseCost, out var baseCost))
+                                {
+                                    basicCost = Convert.ToDouble(baseCost) * 12;
+                                }
+                                if (row.TryGetValue(Constants.AdditionalKwhCost, out var additionalKwhCost))
+                                {
+                                    consumptionCost = Convert.ToDouble(additionalKwhCost) * Convert.ToDouble(consumption);
+                                }
+                            }
+                            else if (type.ToString().Equals(Constants.TariffTypePackaged, StringComparison.OrdinalIgnoreCase))
+                            {
+                                if (row.TryGetValue(Constants.IncludedKwh, out var includedKwh))
+                                {
+                                    if (Convert.ToDouble(includedKwh) >= Convert.ToDouble(consumption))
+                                    {
+                                        if (row.TryGetValue(Constants.BaseCost, out var baseCost))
+                                        {
+                                            basicCost = Convert.ToDouble(baseCost);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        if (row.TryGetValue(Constants.BaseCost, out var baseCost))
+                                        {
+                                            basicCost = Convert.ToDouble(baseCost);
+                                        }
+                                        double difference = Convert.ToDouble(consumption) - Convert.ToDouble(includedKwh);
+                                        if (row.TryGetValue(Constants.AdditionalKwhCost, out var additionalKwhCost))
+                                        {
+                                            consumptionCost = Convert.ToDouble(additionalKwhCost) * difference;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else if (tarifftype.Equals(Constants.TariffTypeBasic))
+                {
+                    if (row.TryGetValue(Constants.BaseCost, out var baseCost))
+                    {
+                        basicCost = Convert.ToDouble(baseCost) * 12;
+                    }
+                    if (row.TryGetValue(Constants.AdditionalKwhCost, out var additionalKwhCost))
+                    {
+                        consumptionCost = Convert.ToDouble(consumption) * Convert.ToDouble(additionalKwhCost);
+                    }
+                }
+                else if (tarifftype.Equals(Constants.TariffTypePackaged))
+                {
+                    if (row.TryGetValue(Constants.IncludedKwh, out var includedKwh))
+                    {
+                        if (Convert.ToDouble(includedKwh) >= Convert.ToDouble(consumption))
                         {
                             if (row.TryGetValue(Constants.BaseCost, out var baseCost))
                             {
-                                basicCost = Convert.ToDouble(baseCost) * 12;
+                                basicCost = Convert.ToDouble(baseCost);
                             }
+                        }
+                        else
+                        {
+                            if (row.TryGetValue(Constants.BaseCost, out var baseCost))
+                            {
+                                basicCost = Convert.ToDouble(baseCost);
+                            }
+                            double difference = Convert.ToDouble(consumption) - Convert.ToDouble(includedKwh);
                             if (row.TryGetValue(Constants.AdditionalKwhCost, out var additionalKwhCost))
                             {
-                                consumptionCost = Convert.ToDouble(additionalKwhCost) * Convert.ToDouble(consumption);
-                            }
-                        }
-                        else if (type.ToString().Equals(Constants.TariffTypePackaged, StringComparison.OrdinalIgnoreCase))
-                        {
-                            if (row.TryGetValue(Constants.IncludedKwh, out var includedKwh))
-                            {
-                                if (Convert.ToDouble(includedKwh) >= Convert.ToDouble(consumption))
-                                {
-                                    if (row.TryGetValue(Constants.BaseCost, out var baseCost))
-                                    {
-                                        basicCost = Convert.ToDouble(baseCost);
-                                    }
-                                }
-                                else
-                                {
-                                    if (row.TryGetValue(Constants.BaseCost, out var baseCost))
-                                    {
-                                        basicCost = Convert.ToDouble(baseCost);
-                                    }
-                                    double difference = Convert.ToDouble(consumption) - Convert.ToDouble(includedKwh);
-                                    if (row.TryGetValue(Constants.AdditionalKwhCost, out var additionalKwhCost))
-                                    {
-                                        consumptionCost = Convert.ToDouble(additionalKwhCost) * difference;
-                                    }
-                                }
+                                consumptionCost = Convert.ToDouble(additionalKwhCost) * difference;
                             }
                         }
                     }
                 }
-            }
-            else if (tarifftype.Equals(Constants.TariffTypeBasic))
-            {
-                if (row.TryGetValue(Constants.BaseCost, out var baseCost))
+                else
                 {
-                    basicCost = Convert.ToDouble(baseCost) * 12;
-                }
-                if (row.TryGetValue(Constants.AdditionalKwhCost, out var additionalKwhCost))
-                {
-                    consumptionCost = Convert.ToDouble(consumption) * Convert.ToDouble(additionalKwhCost);
+                    return 0.00;
                 }
             }
-            else if (tarifftype.Equals(Constants.TariffTypePackaged))
-            {
-                if (row.TryGetValue(Constants.IncludedKwh, out var includedKwh))
-                {
-                    if (Convert.ToDouble(includedKwh) >= Convert.ToDouble(consumption))
-                    {
-                        if (row.TryGetValue(Constants.BaseCost, out var baseCost))
-                        {
-                            basicCost = Convert.ToDouble(baseCost);
-                        }
-                    }
-                    else
-                    {
-                        if (row.TryGetValue(Constants.BaseCost, out var baseCost))
-                        {
-                            basicCost = Convert.ToDouble(baseCost);
-                        }
-                        double difference = Convert.ToDouble(consumption) - Convert.ToDouble(includedKwh);
-                        if (row.TryGetValue(Constants.AdditionalKwhCost, out var additionalKwhCost))
-                        {
-                            consumptionCost = Convert.ToDouble(additionalKwhCost) * difference;
-                        }
-                    }
-                }
-            }
-            else
-            {
-                return 0.00;
-            }
+        }
+        catch (Exception ex)
+        {
+            throw new InvalidOperationException("Error calculating annual cost.", ex);
         }
 
         return basicCost + consumptionCost;
