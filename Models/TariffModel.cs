4110169911b737db312935b82102386de8de9076
Paras Jadav <99166408@brighthorizons.com>
2025-07-10 12:05:22 +0530

    Consolidated .gitignore files into a single solution-wide .gitignore

--- Models/TariffModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TariffComparison.Models
-{
-    public class TariffModel
-    {
-        public string TariffName { get; set; }
-        public string TariffType { get; set; }
-        public double AnnualCost { get; set; }
-    }
-}
